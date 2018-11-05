28f3e89bd8a02a01e8761672e16feae96e390396
esoemad5 <esoemad5@gmail.com>
2018-11-05 08:43:02 -0600

    Created Venue Model

---
+++ Integration_Project/Models/Venue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Models
+{
+    public class Venue
+    {
+        [Key]
+        public string Id { get; set; }
+        // FK CreatedBy
+        public string Name { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsPrivate { get; set; }
+        public string WebsiteUrl { get; set; }
+    }
+}
