76c1528a50fd3f0ff46b2724f282b9e50389f1ba
coltonsells <coltonsells3@yahoo.com>
2018-11-05 14:32:00 -0600

    added eventinterest table

--- Integration_Project/Data/Migrations/20181105203032_added eventInterest table.Designer.cs
+++ Integration_Project/Data/Migrations/20181105203032_added eventInterest table.Designer.cs
@@ -4,14 +4,16 @@ using Integration_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Integration_Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181105203032_added eventInterest table")]
+    partial class addedeventInteresttable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Integration_Project/Models/EventInterest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Models
+{
+    public class EventInterest
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [ForeignKey("Events")]
+        public string EventId { get; set; }
+        public Event Events { get; set; }
+
+        [ForeignKey("Interests")]
+        public string InterestId { get; set; }
+        public Interest Interests { get; set; }
+    }
+}
+
