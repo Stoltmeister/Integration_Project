1e424fcc71e3033c13cf83099ef974c9aa4ef31e
esoemad5 <esoemad5@gmail.com>
2018-11-05 08:46:04 -0600

    Created Interest model

---
+++ Integration_Project/Models/Interest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Models
+{
+    public class Interest
+    {
+        [Key]
+        public string Id { get; set; }
+        // FK Created by
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Verified { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
