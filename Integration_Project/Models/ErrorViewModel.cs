79e5332715c97069916b6076ee338ed6a342bf57
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-11-05 07:45:08 -0600

    initial commit

---
+++ Integration_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Integration_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Integration_Project/Models/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Models
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public int VenueId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Desciption { get; set; }
+        public int Premium { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsWeatherDependent { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
