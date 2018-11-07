03c1105061f4545cb847f6781923a9f6994ca215
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-11-07 10:41:00 -0600

    Creating Interests in VenuesController/Flow now works

---
+++ Integration_Project/Models/VenueInterestCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Models
+{
+    public class VenueInterestCreator
+    {
+        public string CurrentVenueID { get; set; }
+        public Interest NewInterest { get; set; }
+    }
+}
